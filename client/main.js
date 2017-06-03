d361fad705fb562e157e14939055dad55b6d6485
vinh.nguyen <vinh.nguyen@s2corp.vn>
2017-06-03 10:39:08 +0700

    tmp

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from '../imports/ui/App.jsx';
+import App from '../imports/components/App.jsx';
 // import '../imports/ui/module/test';
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
