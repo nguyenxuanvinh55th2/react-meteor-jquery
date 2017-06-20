6600e79901d06e0b13fd10cd52c0d2c4475b3008
vinh.nguyen <vinh.nguyen@s2corp.vn>
2017-06-20 12:50:36 +0700

    tmp

--- imports/components/home/Home.jsx
+++ imports/components/home/Home.jsx
@@ -28,10 +28,44 @@ export default class Home extends React.Component {
     console.log(this.props);
     return (
       <div>
-        <button onClick={() => {
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<div id="myModal" className="modal fade" role="dialog">
+  <div className="modal-dialog">
+
+    <div className="modal-content">
+      <div className="modal-header">
+        <button type="button" className="close" data-dismiss="modal">&times;</button>
+        <h4 className="modal-title">Modal Header</h4>
+      </div>
+      <div className="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+        <div className="dropdown">
+          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Dropdown
+            <span className="caret"></span>
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" className="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </div>
+        <button className="btn btn-primary" onClick={() => {
           this.handle();
-        }}>Add</button>
-        <a href="/about">About</a>
+        }}>Add123456</button>
+        <a href="/about">About vinh nguyen this sao may che det mia may ce</a>
       </div>
     )
   }
